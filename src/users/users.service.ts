3734efb1372e7fffea30227790056e8ae2bdc5fe
nrmadi02 <ahmadinur2202@gmail.com>
2024-03-29 09:52:50 +0800

    [feat]: initial commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+
+
+  create(createUser: CreateUserDto) {
+    const user = this.userRepository.create(createUser);
+    return this.userRepository.save(user);
+  }
+}
